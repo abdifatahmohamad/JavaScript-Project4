62b5899ba8bfb22ca2638b43d2150196ddc18a8a
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-03-17 00:00:52 -0500

    Update lht_table.js

--- js/lht_table.js
+++ js/lht_table.js
@@ -1,16 +1,5 @@
 "use strict";
 
-/*
-   New Perspectives on HTML5 and CSS3, 7th Edition
-   Tutorial 10
-   Review Assignment
-
-   Author: Abdifatah Ali
-   Date:   January 22, 2020
-
-
-*/
-
 /* Set the date displayed in the calendar */
 var thisDay = new Date("August 30, 2018");
 
